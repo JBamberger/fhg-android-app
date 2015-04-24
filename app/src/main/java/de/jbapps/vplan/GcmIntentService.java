2d7c033e9b88f6990b4935f3b2dc3cfab8782ddb
Jannik Bamberger <dev.jbamberger@gmail.com>
2015-04-24 15:02:41 +0200

    Changed notification behaviour

--- app/src/main/java/de/jbapps/vplan/GcmIntentService.java
+++ app/src/main/java/de/jbapps/vplan/GcmIntentService.java
@@ -16,10 +16,9 @@ import android.util.Log;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmIntentService extends IntentService {
-    public static final int NOTIFICATION_ID = 1;
+    public static final int NOTIFICATION_ID_VPLAN = 1;
+    public static final int NOTIFICATION_ID_APP_UPDATE = 2;
     private static final String TAG = "GcmIntentService";
-    NotificationCompat.Builder builder;
-    private NotificationManager mNotificationManager;
 
     public GcmIntentService() {
         super("GcmIntentService");
@@ -29,59 +28,49 @@ public class GcmIntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         Bundle extras = intent.getExtras();
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-        // The getMessageType() intent parameter must be the intent you received
-        // in your BroadcastReceiver.
         String messageType = gcm.getMessageType(intent);
 
-        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
-            /*
-             * Filter messages based on message type. Since it is likely that GCM
-             * will be extended in the future with new message types, just ignore
-             * any message types you're not interested in, or that you don't
-             * recognize.
-             */
-            if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " +
-                        extras.toString());
-                // If it's a regular GCM message, do some work.
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+        if (!extras.isEmpty()) {
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                Log.i(TAG, "Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                Log.i(TAG, "Deleted messages on server: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
 
-                // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
+                sendNotification(NOTIFICATION_ID_VPLAN, "VPlan aktualisiert", "Received: " + extras.toString());
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
-        // Release the wake lock provided by the WakefulBroadcastReceiver.
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
-    // Put the message into a notification and post it.
-    // This is just one simple example of what you might choose to do with
-    // a GCM message.
-    private void sendNotification(String msg) {
+    private void sendNotification(int Id, String title, String msg) {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         if (pref.getBoolean("notification_enabled", true)) {
-
             NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, VPlanActivity.class), 0);
+
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
                     .setSmallIcon(R.drawable.ic_notification)
-                    .setContentTitle("VPlan Trigger occurred")
+                    .setContentTitle(title)
                     .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                     .setContentText(msg)
                     .setTicker(msg)
-                    .setSound(Uri.parse(pref.getString("notification_ringtone", "DEFAULT_SOUND")))
-                    .setContentIntent(contentIntent);
+                    .setSound(Uri.parse(pref.getString("notification_ringtone", "DEFAULT_SOUND")));
+
+
+            if (Id == NOTIFICATION_ID_VPLAN) {
+                PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, VPlanActivity.class), 0);
+                mBuilder.setContentIntent(contentIntent);
+            } else if (Id == NOTIFICATION_ID_APP_UPDATE) {
+                PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.de/")), 0); //TODO: VPlan update page
+                mBuilder.setContentIntent(contentIntent);
+            }
 
             if (pref.getBoolean("notification_vibrate", true)) {
                 long[] pattern = {500, 500, 400, 650};
                 mBuilder.setVibrate(pattern);
             }
+
             String color = pref.getString("notification_color", "#00ff00");
             if ("#FF0000".equals(color)) {
                 mBuilder.setLights(Color.RED, 1000, 1000);
@@ -90,7 +79,7 @@ public class GcmIntentService extends IntentService {
             } else if ("#0000FF".equals(color)) {
                 mBuilder.setLights(Color.BLUE, 1000, 1000);
             }
-            mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+            mNotificationManager.notify(Id, mBuilder.build());
         }
     }
 }
