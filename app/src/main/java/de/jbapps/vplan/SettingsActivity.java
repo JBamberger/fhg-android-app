9c514c448daaf5cfd8259df39eae48e6e8cbc2d2
Jannik Bamberger <dev.jbamberger@gmail.com>
2015-05-19 21:50:00 +0200

    Build 1.2.3 setting changes v1

--- app/src/main/java/de/jbapps/vplan/SettingsActivity.java
+++ app/src/main/java/de/jbapps/vplan/SettingsActivity.java
@@ -1,9 +1,8 @@
 package de.jbapps.vplan;
 
 import android.annotation.TargetApi;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -12,139 +11,131 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.jbapps.vplan.util.Property;
 
 public class SettingsActivity extends PreferenceActivity {
 
-    private static final boolean ALWAYS_SIMPLE_PREFS = false;
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
             String stringValue = value.toString();
 
             if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
                 ListPreference listPreference = (ListPreference) preference;
                 int index = listPreference.findIndexOfValue(stringValue);
 
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
 
             } else if (preference instanceof RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
                 if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
                     preference.setSummary(R.string.pref_ringtone_silent);
-
                 } else {
                     Ringtone ringtone = RingtoneManager.getRingtone(
                             preference.getContext(), Uri.parse(stringValue));
 
                     if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
                         preference.setSummary(null);
                     } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
                         String name = ringtone.getTitle(preference.getContext());
                         preference.setSummary(name);
                     }
                 }
 
             } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
                 preference.setSummary(stringValue);
             }
             return true;
         }
     };
 
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
+    static Preference.OnPreferenceClickListener gradeListener = new Preference.OnPreferenceClickListener() {
+        @Override
+        public boolean onPreferenceClick(final Preference preference) {
+            final List<Integer> selected = new ArrayList<>();
+            AlertDialog.Builder builder = new AlertDialog.Builder(preference.getContext());
+            builder.setTitle(R.string.text_dialog_pick_grade)
+                    .setMultiChoiceItems(R.array.listGrades, null,
+                            new DialogInterface.OnMultiChoiceClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which,
+                                                    boolean isChecked) {
+                                    if (isChecked) {
+                                        selected.add(which);
+                                    } else if (selected.contains(which)) {
+                                        selected.remove(Integer.valueOf(which));
+                                    }
+                                }
+                            })
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            String[] list = preference.getContext().getResources().getStringArray(R.array.listGrades);
+                            StringBuilder output = new StringBuilder();
+                            for (int i : selected) {
+                                output.append(list[i]);
+                                output.append(",");
+                            }
+                            String grades = output.toString();
+                            grades = grades.substring(0, grades.length() - 1);
+                            new Property(preference.getContext()).storeGrade(grades);
+                        }
+                    })
+                    .setNegativeButton(R.string.select_all, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            new Property(preference.getContext()).storeGrade("");
+                        }
+                    });
 
-    private static boolean isSimplePreferences(Context context) {
-        return ALWAYS_SIMPLE_PREFS
-                || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
-                || !isXLargeTablet(context);
-    }
+            builder.create().show();
+            return false;
+        }
+    };
 
     private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
         sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
     }
+
+    final static String ACTION_PREFS_GENERAL = "de.jbapps.prefs.PREFS_GENERAL";
+    final static String ACTION_PREFS_NOTIFICATION = "de.jbapps.prefs.PREFS_NOTIFICATION";
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        setupSimplePreferencesScreen();
-    }
-
-    private void setupSimplePreferencesScreen() {
-        if (!isSimplePreferences(this)) {
-            return;
+        String action = getIntent().getAction();
+        if (action != null && action.equals(ACTION_PREFS_GENERAL)) {
+            addPreferencesFromResource(R.xml.pref_general);
+            findPreference("grade").setOnPreferenceClickListener(gradeListener);
+            try {
+                findPreference("version").setSummary(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else if (action != null && action.equals(ACTION_PREFS_NOTIFICATION)) {
+            addPreferencesFromResource(R.xml.pref_notification);
+            bindPreferenceSummaryToValue(findPreference("notification_ringtone"));
+            bindPreferenceSummaryToValue(findPreference("notification_color"));
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            addPreferencesFromResource(R.xml.pref_header_legacy);
         }
-
-        // In the simplified UI, fragments are not used at all and we instead
-        // use the older PreferenceActivity APIs.
-
-        // Add 'general' preferences.
-        addPreferencesFromResource(R.xml.pref_general);
-
-        // Add 'notifications' preferences, and a corresponding header.
-        PreferenceCategory fakeHeader = new PreferenceCategory(this);
-        fakeHeader.setTitle(R.string.pref_header_notifications);
-        getPreferenceScreen().addPreference(fakeHeader);
-        addPreferencesFromResource(R.xml.pref_notification);
-
-
-        // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
-        // their values. When their values change, their summaries are updated
-        // to reflect the new value, per the Android Design guidelines.
-        bindPreferenceSummaryToValue(findPreference("notification_ringtone"));
-        bindPreferenceSummaryToValue(findPreference("notification_color"));
-        try {
-            findPreference("version").setSummary(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onIsMultiPane() {
-        return isXLargeTablet(this) && !isSimplePreferences(this);
     }
 
     @Override
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void onBuildHeaders(List<Header> target) {
-        if (!isSimplePreferences(this)) {
-            loadHeadersFromResource(R.xml.pref_headers, target);
-        }
+        loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -153,6 +144,13 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
+
+            findPreference("grade").setOnPreferenceClickListener(gradeListener);
+            try {
+                findPreference("version").setSummary(getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -166,5 +164,21 @@ public class SettingsActivity extends PreferenceActivity {
             bindPreferenceSummaryToValue(findPreference("notification_ringtone"));
             bindPreferenceSummaryToValue(findPreference("notification_color"));
         }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class NotificationPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_notification);
+
+            bindPreferenceSummaryToValue(findPreference("notification_ringtone"));
+            bindPreferenceSummaryToValue(findPreference("notification_color"));
+        }
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        return GeneralPreferenceFragment.class.getName().equals(fragmentName) || NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
 }
