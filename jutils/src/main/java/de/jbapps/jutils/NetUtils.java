effd6d6cc5b75b1e0610851cbb5ce2c795883170
Jannik Bamberger <dev.jbamberger@gmail.com>
2015-05-25 23:19:21 +0200

    extended Net and ViewUtils

--- jutils/src/main/java/de/jbapps/jutils/NetUtils.java
+++ jutils/src/main/java/de/jbapps/jutils/NetUtils.java
@@ -1,5 +1,9 @@
 package de.jbapps.jutils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import org.apache.http.NameValuePair;
 
 import java.io.UnsupportedEncodingException;
@@ -23,5 +27,32 @@ public class NetUtils {
             result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
         }
         return result.toString();
+    }
+
+    /**
+     * Checks to see if the device is connected to a network (cell, wifi, etc).
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network.
+     */
+    public static boolean isConnectedWifi(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     */
+    public static boolean isConnectedMobile(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
     }
 }
