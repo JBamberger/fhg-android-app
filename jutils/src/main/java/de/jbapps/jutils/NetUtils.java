3f2365124d48605a2ea32020b9b7dfbad578242b
Jannik Bamberger <dev.jbamberger@gmail.com>
2016-03-25 21:46:18 +0100

    updated further dependencies and build tools, removed Netutils.getQuery

--- jutils/src/main/java/de/jbapps/jutils/NetUtils.java
+++ jutils/src/main/java/de/jbapps/jutils/NetUtils.java
@@ -4,30 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import org.apache.http.NameValuePair;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.List;
-
 public class NetUtils {
-
-    public static String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-
-        for (NameValuePair pair : params) {
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
-        }
-        return result.toString();
-    }
 
     /**
      * Checks to see if the device is connected to a network (cell, wifi, etc).
