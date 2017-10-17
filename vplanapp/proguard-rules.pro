a1cf6b8f72829469f81b16c7b84eb80ec2d49f11
Jannik Bamberger <dev.jbamberger@gmail.com>
2017-10-17 18:31:52 +0200

    shrink and minify

--- vplanapp/proguard-rules.pro
+++ vplanapp/proguard-rules.pro
@@ -1,27 +1,13 @@
-# Add project specific ProGuard rules here.
-# By default, the flags in this file are appended to flags specified
-# in C:\Users\Jannik\Tools\Android\sdk/tools/proguard/proguard-android.txt
-# You can edit the include path and order by changing the proguardFiles
-# directive in build.gradle.
-#
-# For more details, see
-#   http://developer.android.com/guide/developing/tools/proguard.html
 
-# Add any project specific keep options here:
-
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
+# jsoup
+-keeppackagenames org.jsoup.nodes
 
 
--verbose
+# gson
 
--keep class android.support.v7.** { *; }
--keep interface android.support.v7.** { *; }
--keep class xyz.jbapps.vplan.** {*;}
--keep interface xyz.jbapps.vplan.** {*;}
--keep class android.support.v7.** { *; }
--keep interface android.support.v7.** { *; }
+# Gson uses generic type information stored in a class file when working with fields. Proguard
+# removes such information by default, so configure it to keep all of it.
+-keepattributes Signature
+
+# for gsons @SerializedName
+-keepattributes *Annotation*
--- vplanapp/src/main/java/xyz/jbapps/vplan/ui/activity/BaseActivity.java
+++ vplanapp/src/main/java/xyz/jbapps/vplan/ui/activity/BaseActivity.java
@@ -1,31 +1,28 @@
 package xyz.jbapps.vplan.ui.activity;
 
-import android.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 import de.jbapps.jutils.ViewUtils;
 import xyz.jbapps.vplan.R;
 import xyz.jbapps.vplan.ui.fragment.ContactFragment;
 import xyz.jbapps.vplan.ui.fragment.CreditsFragment;
 import xyz.jbapps.vplan.ui.fragment.FHGFeedFragment;
-import xyz.jbapps.vplan.ui.fragment.SettingsFragment;
 import xyz.jbapps.vplan.ui.fragment.VPlanFragment;
 
 public class BaseActivity extends AppCompatActivity {
@@ -60,6 +57,9 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "Created Activity");
+
         setContentView(R.layout.activity_base);
         setupUI();
         if (savedInstanceState != null) {
