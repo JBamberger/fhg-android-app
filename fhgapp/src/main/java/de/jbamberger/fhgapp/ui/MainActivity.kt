352be2edfd53a49d3f14c1b6c464d1db15b9457f
Jannik Bamberger <dev.jbamberger@gmail.com>
2017-12-29 21:55:24 +0100

    fixed kotlin build error and deleted old main activity

--- fhgapp/src/main/java/de/jbamberger/fhgapp/ui/MainActivity.kt
+++ fhgapp/src/main/java/de/jbamberger/fhgapp/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package de.jbamberger.fhgapp.ui
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
 import de.jbamberger.fhgapp.R
+import de.jbamberger.fhgapp.databinding.ActivityMainBinding
 import de.jbamberger.fhgapp.ui.components.BaseActivity
-import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
-class Main2Activity : BaseActivity() {
+class MainActivity : BaseActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -26,10 +31,31 @@ class Main2Activity : BaseActivity() {
         false
     }
 
+    @Inject
+    @JvmField
+    var viewModel: MainViewModel? = null
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item == null) return super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_share -> return true
+            R.id.action_settings -> return true
+            R.id.action_about -> return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
