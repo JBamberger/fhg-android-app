a7f883bccd3ace3ad35b023027497083d9098023
Jannik Bamberger <dev.jbamberger@gmail.com>
2021-09-30 23:20:43 +0200

    Bump gradle plugin and dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val kotlinVersion by extra("1.5.10")
-    val hiltVersion by extra("2.37")
+    val hiltVersion by extra("2.39")
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         classpath("app.cash.licensee:licensee-gradle-plugin:1.1.0")
