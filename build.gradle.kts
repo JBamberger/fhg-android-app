b7258ea78fdb7d9ed98d7e2245c1d242dcc0d491
Jannik Bamberger <mail@jbamberger.de>
2023-08-19 17:55:29 +0200

    Update to targetSdk 31, Kotlin 1.8.2, AGP 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.6.0")
-    val hiltVersion by extra("2.39")
+    val kotlinVersion by extra("1.8.20")
+    val hiltVersion by extra("2.44")
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         classpath("app.cash.licensee:licensee-gradle-plugin:1.1.0")
