d4d640d006bb262569469840ed50b6161d49ddda
Jannik Bamberger <mail@jbamberger.de>
2023-08-20 18:12:13 +0200

    Update targetSdk to 33, AGP to 8.1.0 and most dependencies to the current version.
    This required setting up a lint rule to ignore an error caused by a Glide class that is not in use.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.8.20")
+    val kotlinVersion by extra("1.8.21")
     val hiltVersion by extra("2.44")
 
     repositories {
@@ -8,7 +8,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         classpath("app.cash.licensee:licensee-gradle-plugin:1.1.0")
@@ -20,4 +20,8 @@ allprojects {
         mavenCentral()
         google()
     }
+}
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
 }
